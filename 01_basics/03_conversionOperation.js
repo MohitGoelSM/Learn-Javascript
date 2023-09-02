073a5ce504dc08c3f808455b386d61189c42e2ff
MohitGoelSM <140971774+MohitGoelSM@users.noreply.github.com>
2023-09-02 15:36:34 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,42 @@
+// let score = undefined;
+
+// console.log(typeof score);
+
+// let valueInNumber=Number(score);//convert the string to number
+
+// console.log(typeof valueInNumber); 
+// /*
+// score   typeof(valueInNumber)   valueInNumber
+// "33"        number                      33
+// "23abc"     number                      NaN
+// null        number                      0                      
+// undefined   number                      NaN
+// true        number                      1
+// "abc"       number                      NaN
+// */
+
+// console.log(valueInNumber);
+
+
+// let isLoggedIn=undefined;
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// //1 => true
+// //0 => false
+// //""=> false
+// //"a"=> true
+// //null=> false
+// //undefine=> false
+
+
+let someValue=true
+let ConvertedToString=String(someValue);
+console.log(typeof ConvertedToString);
+console.log(ConvertedToString);
+// 33      =>      33
+// null    =>      null
+//undefined=>      undefined
+//true     =>      true 
